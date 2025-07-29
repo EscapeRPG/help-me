b44d97f9477ba950f89cd600c88c2dcd21991efd
EscapeRPG <syrphin@syrphin.com>
2025-07-29 15:52:40 +0200

    S003 terminé

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT_NO || 3000;
 
+app.use(express.static("public"));
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+let variableB =
+  "Camille est une super camarade, je l'aime beaucoup elle est super sympa, en plus ses cheveux courts ça lui va super bien.";
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index", {
+    variable: variableB,
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
+app.listen(port);
